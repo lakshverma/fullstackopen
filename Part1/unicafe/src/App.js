e38ef28da2682cd2bbe589a1dec72b318df62c92
Lakshay Verma <lakshverma23@gmail.com>
2021-09-07 23:38:49 +0530

    refactor components and render data output in a table

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,44 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // Component to generate Section headers
-const Header = ({text}) => <h1>{text}</h1>
+const Header = ({ text }) => <h1>{text}</h1>;
 
 // Component to generate feedback buttons with click eventhandlers attached
-const Button = ({handleClick, buttonText}) => {
-  return (
-  <button onClick={handleClick}> {buttonText} </button>
-  )
-}
+const Button = ({ handleClick, buttonText }) => {
+  return <button onClick={handleClick}> {buttonText} </button>;
+};
 
 // Component that prints the aggregate statistics
-const Total = (props) => {
-  return (
-    <div>
-      <p>good {props.count[0]}</p>
-      <p>neutral {props.count[1]}</p>
-      <p>bad {props.count[2]}</p>
-    </div>
-  )
-}
+const StatisticLine = ({ text, value }) => {
+  // const [good, neutral, bad] = count;
+  // const total = good + neutral + bad
+
+  if (value === 0 || value === "NaN%") {
+    return (
+      <tr>
+        <td>{text} No feedback given</td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>
+          {text} {value}{" "}
+        </td>
+      </tr>
+    );
+  }
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   // Function to execute when a feedback button is clicked. Also updates the state for clicks of each button.
   const clickHandler = (button) => () => {
-    if (button === 'good') {
-      setGood(good+1);
-    } else if (button === 'neutral') {
-      setNeutral(neutral+1);
+    if (button === "good") {
+      setGood(good + 1);
+    } else if (button === "neutral") {
+      setNeutral(neutral + 1);
     } else {
-      setBad(bad+1);
+      setBad(bad + 1);
     }
-  }
+  };
 
-  const clickCount = [good, neutral, bad];
+  const total = good + neutral + bad;
 
   return (
     <div>
@@ -47,9 +56,19 @@ const App = () => {
       <Button handleClick={clickHandler("neutral")} buttonText="neutral" />
       <Button handleClick={clickHandler("bad")} buttonText="bad" />
       <Header text="statistics" />
-      <Total count={clickCount} />
-    </div>
-  )
-  }
 
-export default App
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="total" value={total} />
+          <StatisticLine text="average" value={total / 3} />
+          <StatisticLine text="positive" value={(good / total) * 100 + "%"} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
