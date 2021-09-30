3ff835510dd4835e3a15d1c5d7930f2e9cccfa72
Lakshay Verma <lakshverma23@gmail.com>
2021-09-30 12:27:05 +0530

    Change backend address to relative URL and fix frotend dev mode using a proxy

--- Part2/phonebook/src/services/people.js
+++ Part2/phonebook/src/services/people.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/people";
+const baseUrl = "/api/people";
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -10,6 +10,7 @@ const create = (newObject) => {
     const request = axios.post(baseUrl, newObject)
     return request.then(response => response.data)};
 
+// Currently broken, fix in exercise 3.17    
 const update = (id, newObject) => {
     return axios.put(`${baseUrl}/${id}`, newObject)
 };
